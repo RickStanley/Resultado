82292d7141a0fc8b0340b8172407fe73543208ec
Rick Stanley Mota da Cunha <rick.cunha@tivit.com>
2025-06-02 20:13:12 +0000

    feat(resultweb): adiciona extensões para falhas
    feat(resultweb): adiciona extensões para falhas

---
+++ Resultado.Web/JsonPointerHelper.cs
@@ -0,0 +1,140 @@
+﻿namespace Resultado.Web;
+
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+/// <summary>
+/// Auxílio conversão de expressão LINQ em ponteiro JSON, de acordo com https://www.rfc-editor.org/rfc/rfc6901.html
+/// </summary>
+public static class JsonPointerHelper
+{
+    private static string GetSerializedPropertyName(MemberInfo member, JsonSerializerOptions? options)
+    {
+        JsonPropertyNameAttribute? jsonProperty = member.GetCustomAttribute<JsonPropertyNameAttribute>();
+        if (jsonProperty != null)
+        {
+            return jsonProperty.Name;
+        }
+
+        // Aplica política de nomenclatura, se definido
+        string propertyName = member.Name;
+        return options?.PropertyNamingPolicy?.ConvertName(propertyName) ?? propertyName;
+    }
+
+    /// <summary>
+    /// Pega o ponteiro JSON de um membro, em representação URI (RFC 6901 §5).
+    /// </summary>
+    /// <param name="expression">Expressão LINQ para criar o ponteiro desejado.</param>
+    /// <param name="options">Opções de serialização, usada para especificar a nomenclatura.</param>
+    /// <typeparam name="T">Tipo alvo a ser inspecionado.</typeparam>
+    /// <returns>Cadeia de caracteres representação JSON. <example>"#/Example3Array/0/Nested/1"</example></returns>
+    public static string GetJsonUriPointer<T>(Expression<Func<T, object>> expression,
+        JsonSerializerOptions? options = null)
+        => GetJsonPointer(expression, JsonPointerRepresentation.UriFragment, options);
+
+    public static string GetJsonPointer<T>(Expression<Func<T, object>> expression,
+        JsonPointerRepresentation pointerRepresentation, JsonSerializerOptions? options = null)
+    {
+        if (pointerRepresentation == JsonPointerRepresentation.Normal)
+            throw new NotImplementedException();
+
+        Stack<string> pathParts = GetJsonPointerParts(expression, options);
+
+        if (pathParts.Count == 0)
+            return pointerRepresentation == JsonPointerRepresentation.UriFragment ? "#" : "";
+
+        string result = pointerRepresentation == JsonPointerRepresentation.UriFragment
+            ? "#/" + string.Join('/', pathParts.Select(Uri.EscapeDataString))
+            : "/" + string.Join('/', pathParts);
+
+        return result;
+    }
+
+    public static string GetJsonPointer<T>(Expression<Func<T, object>> expression,
+        JsonSerializerOptions? options = null)
+    {
+        Stack<string> pathParts = GetJsonPointerParts(expression, options);
+        return "/" + string.Join("/", pathParts);
+    }
+
+    private static Stack<string> GetJsonPointerParts<T>(Expression<Func<T, object>> expression,
+        JsonSerializerOptions? options)
+    {
+        const string safetyFallback = "INVALID_EXPRESSION";
+        Stack<string> pathParts = new();
+
+        Expression? currentExpression = expression.Body;
+        while (currentExpression is not ParameterExpression)
+        {
+            switch (currentExpression)
+            {
+                case UnaryExpression unaryExpression:
+                    // Expressões em cima de números são unários.
+                    currentExpression = unaryExpression.Operand;
+                    continue;
+                case MemberExpression memberExpression:
+                    // Acesso de membro: pega o nome serializado e pop
+                    pathParts.Push(GetSerializedPropertyName(memberExpression.Member, options));
+                    currentExpression = memberExpression.Expression;
+                    break;
+                case BinaryExpression { Right: ConstantExpression arrayIndexConstantExpression } binaryExpression and
+                    { NodeType: ExpressionType.ArrayIndex }:
+                    string? item = arrayIndexConstantExpression.Value?.ToString();
+                    if (item is null)
+                        return new Stack<string>([safetyFallback]);
+                    // Índice de array
+                    pathParts.Push(item);
+                    currentExpression = binaryExpression.Left;
+                    break;
+                case MethodCallExpression
+                    {
+                        Arguments:
+                        [
+                            ConstantExpression
+                            {
+                                Type.Name: nameof(Int32)
+                            } listIndexConstantExpression
+                        ],
+                        Method.Name: "get_Item"
+                    } callExpression
+                    when callExpression.Method.DeclaringType.GetInterfaces().Any(i =>
+                        i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IReadOnlyList<>)):
+                    // IReadOnlyList índice de outro tipo
+                    string? index = listIndexConstantExpression.Value?.ToString();
+                    if (index is null)
+                        return new Stack<string>([safetyFallback]);
+                    pathParts.Push(index);
+                    currentExpression = callExpression.Object;
+                    break;
+                default:
+                    throw new InvalidOperationException(
+                        $"{currentExpression?.GetType().Name} (at {currentExpression}) not supported");
+            }
+        }
+
+        return pathParts;
+    }
+}
+
+/// <summary>
+/// Valores que especificam a representação de um ponteiro JSON.
+/// </summary>
+public enum JsonPointerRepresentation
+{
+    /// <summary>
+    /// A representação especificada em RFC 6901 §3.
+    /// </summary>
+    Normal = 0,
+
+    /// <summary>
+    /// A representação cadeia de caracteres JSON especificada  em RFC 6901 §5
+    /// </summary>
+    JsonString,
+
+    /// <summary>
+    /// A representação fragmento identificador URI especificado em RFC 6901 §6
+    /// </summary>
+    UriFragment
+}
---
+++ Resultado.Web/ResultWeb.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Resultado.Web;
+
+// Inspirado em: https://github.com/ardalis/Result/issues/193
+public static class ResultWeb
+{
+    private const string StatusCodeDocumentationLinkBase =
+        "https://developer.mozilla.org/en-US/docs/Web/HTTP/Reference/Status";
+
+    public static int AsHttpStatusCode(this Kind kind) =>
+        kind switch
+        {
+            Kind.Ok => StatusCodes.Status200OK,
+            Kind.Created => StatusCodes.Status201Created,
+            Kind.NoContent => StatusCodes.Status204NoContent,
+            Kind.Accepted => StatusCodes.Status202Accepted,
+            Kind.Error => StatusCodes.Status500InternalServerError,
+            Kind.Critical => StatusCodes.Status500InternalServerError,
+            Kind.Unavailable => StatusCodes.Status503ServiceUnavailable,
+            Kind.Invalid => StatusCodes.Status400BadRequest,
+            Kind.Unprocessable => StatusCodes.Status422UnprocessableEntity,
+            Kind.Forbidden => StatusCodes.Status403Forbidden,
+            Kind.Unauthorized => StatusCodes.Status401Unauthorized,
+            Kind.Conflict => StatusCodes.Status409Conflict,
+            Kind.NotFound => StatusCodes.Status404NotFound,
+            Kind.FailedDependency => StatusCodes.Status424FailedDependency,
+            _ => throw new ArgumentOutOfRangeException(nameof(kind))
+        };
+
+    public static ProblemDetails AsProblemDetails(
+        this IResult result,
+        string? detail = null,
+        string? instance = null,
+        int? statusCode = null,
+        string? title = null,
+        string? type = null,
+        IDictionary<string, object?>? extensions = null
+    )
+    {
+        IDictionary<string, object?> extensionDictionary = extensions ?? new Dictionary<string, object?>();
+
+        if (result is not IFailedResult failedResult)
+            throw new ArgumentException($"{nameof(result)} must implement {nameof(IFailedResult)}.", nameof(result));
+
+        if (failedResult.ValidationErrors.Count > 0)
+            extensionDictionary.Add("errors",
+                failedResult.ValidationErrors.Select(ve => new { ve.Pointer, ve.Detail }));
+
+        return new ProblemDetails
+        {
+            Title = title ?? failedResult.Title,
+            Detail = detail ?? failedResult.Detail ?? failedResult.Errors.FirstOrDefault(),
+            Status = statusCode ?? failedResult.Kind.AsHttpStatusCode(),
+            Type = type ?? StatusCodeDocumentationLinkBase + "/" + failedResult.Kind.AsHttpStatusCode(),
+            Instance = instance,
+            Extensions = extensionDictionary
+        };
+    }
+}
