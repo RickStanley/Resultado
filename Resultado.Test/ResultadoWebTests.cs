24a865d7bbd45f11f2a5914ce18618d35374c8d5
rick <rick-stanley@outlook.com>
2025-08-19 00:49:31 +0000

    test(web): adiciona testes para extensões web

--- Resultado.Test/ResultadoWebTests.cs
+++ Resultado.Test/ResultadoWebTests.cs
@@ -1,6 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+using Resultado.Web;
+
 namespace Resultado.Test;
 
 public class ResultadoWebTests
 {
-    
+    [Fact]
+    public void Result_WhenSuccess_Throws()
+    {
+        var result = Result.Succeed();
+
+        var act = () => result.AsProblemDetails();
+
+        Assert.Throws<ArgumentException>(act);
+    }
+
+    [Fact]
+    public void Result_WhenFailure_ConvertsToProblemDetails()
+    {
+        var result = Result.Fail(new ValidationError("Some error"));
+        
+        var problemDetails = result.AsProblemDetails();
+        
+        Assert.IsType<ProblemDetails>(problemDetails);
+    }
 }
